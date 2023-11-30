fdcb75b8cac60a366d4ecc19d31613e5b8ee856c
Matias Risco <matiasriscolagos@gmail.com>
2023-11-30 18:01:07 +0100

    Add types of games

---
+++ src/store/feature/games/types.ts
@@ -0,0 +1,104 @@
+type Difficulty =
+  | "Just press 'W'"
+  | "Easy"
+  | "Easy to learn / Hard to master"
+  | "Significant brain usage"
+  | "Difficult"
+  | "Dark Souls";
+
+type Graphics =
+  | "You forget what reality is"
+  | "Beautiful"
+  | "Good"
+  | "Decent"
+  | "Bad"
+  | "Don't look too long at it"
+  | "MS-DOS";
+
+type Grid =
+  | "Nothing to grind"
+  | "Only if u care about leaderboards/ranks"
+  | "Isn't necessary to progress"
+  | "Average grind level"
+  | "Too much grind"
+  | "You'll need a second life for grinding";
+
+type GameTime =
+  | "Long enough for a cup of coffee"
+  | "Short"
+  | "Average"
+  | "Long"
+  | "To infinity and beyond";
+
+type Audience = "Kids" | "Teens" | "Adults" | "Grandma";
+
+type Languages =
+  | "English"
+  | "French"
+  | "German"
+  | "Russian"
+  | "Danish"
+  | "Dutch"
+  | "Finnish"
+  | "Italian"
+  | "Japanese"
+  | "Norwegian"
+  | "Polish";
+
+type Tag =
+  | "Indie"
+  | "Action"
+  | "Adventure"
+  | "Casual"
+  | "RPG"
+  | "Simulation"
+  | "Strategy"
+  | "Singleplayer"
+  | "Early Access"
+  | "Free to Play"
+  | "2D"
+  | "3D"
+  | "Atmospheric"
+  | "Fantasy"
+  | "Story Rich"
+  | "Colorful"
+  | "Multiplayer"
+  | "Puzzle"
+  | "Exploration"
+  | "Pixel Graphics"
+  | "Massively Multiplayer"
+  | "Cute"
+  | "Sports"
+  | "Violent"
+  | "First-Person"
+  | "Combat"
+  | "Racing"
+  | "Action-Adventure"
+  | "Anime"
+  | "Arcade"
+  | "Funny"
+  | "Sci-fi"
+  | "Shooter"
+  | "Relaxing"
+  | "Horror"
+  | "Gore";
+
+type Plataforms = "VR" | "Windows" | "Linux" | "Mac";
+
+export interface GameStructure {
+  id: string;
+  name: string;
+  plataforms: Plataforms[];
+  difficulty: Difficulty;
+  imageUrl: string;
+  languages: Languages[];
+  graphics: Graphics;
+  audience: Audience[];
+  grind: Grid;
+  gameTime: GameTime;
+  tags: Tag[];
+}
+
+export interface GameStateStructure {
+  games: GameStructure[];
+}
