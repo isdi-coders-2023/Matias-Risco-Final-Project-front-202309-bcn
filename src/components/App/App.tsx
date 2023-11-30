6e2b6ea3bdfed1624e08056bf0bc624198cb8a05
Matias Risco <matiasriscolagos@gmail.com>
2023-11-30 16:52:22 +0100

    Implement AppStyled in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,17 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import NavigationBar from "../NavigationBar/NavigationBar";
 import HomePage from "../../pages/HomePage/HomePage";
+import AppStyled from "./AppStyled";
 
 const App = (): React.ReactElement => (
-  <div>
+  <AppStyled>
     <Header />
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<HomePage />} />
     </Routes>
     <NavigationBar />
-  </div>
+  </AppStyled>
 );
 
 export default App;
