0612729fabcf16aabd2f1fcdda00ef2c357722b3
Matias Risco <matiasriscolagos@gmail.com>
2023-11-30 09:49:50 +0100

    Add component Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = (): React.ReactElement => (
+  <header>
+    <img
+      src="./images/valveLogo.svg"
+      alt="logo of valve"
+      width="320"
+      height="88.77"
+      className="title-logo"
+    />
+  </header>
+);
+
+export default Header;
