db8cd42979114be57c858e1439b2fbd018cf2e47
Matias Risco <matiasriscolagos@gmail.com>
2023-12-01 16:46:37 +0100

    Fix button component tags

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,19 +1,43 @@
+import { NavLink } from "react-router-dom";
 import NavigationBarStyled from "./NavigationBarStyled";
 
 const NavigationBar = (): React.ReactElement => (
   <NavigationBarStyled>
-    <button className="button-icon">
-      <img src="images/icon-home.webp" alt="button" width="48" height="48" />
-      Home
-    </button>
-    <button className="button-icon">
-      <img src="images/icon-user.webp" alt="button" width="48" height="48" />
-      Login
-    </button>
-    <button className="button-icon">
-      <img src="images/icon-plus.webp" alt="button" width="48" height="48" />
-      Add
-    </button>
+    <ul>
+      <li>
+        <NavLink className="button-icon" to="/home">
+          <img
+            src="images/icon-home.webp"
+            alt="button"
+            width="48"
+            height="48"
+          />
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="button-icon" to="/login">
+          <img
+            src="images/icon-user.webp"
+            alt="button"
+            width="48"
+            height="48"
+          />
+          Login
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="button-icon" to="/add">
+          <img
+            src="images/icon-plus.webp"
+            alt="button"
+            width="48"
+            height="48"
+          />
+          Add
+        </NavLink>
+      </li>
+    </ul>
   </NavigationBarStyled>
 );
 
