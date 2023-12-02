c96deb4428d1ac30e7d1b97166c02882b97191b8
Matias Risco <matiasriscolagos@gmail.com>
2023-12-02 15:39:08 +0100

    Implement Loading in getGames of hook UseGameApi

--- src/hooks/useGameApi.ts
+++ src/hooks/useGameApi.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { GameStructure } from "../store/feature/games/types";
+import { useAppDispatch } from "../store/hooks";
+import { toggleLoadingActionCreator } from "../store/feature/ui/UiSlice";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 const useGameApi = () => {
+  const dispatch = useAppDispatch();
+
   const getGamesApi = useCallback(async (): Promise<GameStructure[]> => {
+    dispatch(toggleLoadingActionCreator());
+
     const {
       data: { games },
     } = await axios.get<{ games: GameStructure[] }>("/games");
+
+    dispatch(toggleLoadingActionCreator());
+
     return games;
-  }, []);
+  }, [dispatch]);
 
   return { getGamesApi };
 };
