a1947bb358d4c1dd97aa00f18d65e119ed9471b9
Matias Risco <matiasriscolagos@gmail.com>
2023-12-02 20:22:47 +0100

    Add error user feedback of getGamesApi in the hook useGameApi

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,8 @@ import HomePageStyled from "./HomePageStyled";
 import { loadGamesActionCreator } from "../../store/feature/games/GamesSlice";
 import GamesList from "../../components/GamesList/GamesList";
 import useGameApi from "../../hooks/useGameApi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const HomePage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
@@ -11,14 +13,29 @@ const HomePage = (): React.ReactElement => {
 
   useEffect(() => {
     (async () => {
-      const gamesData = await getGamesApi();
-      dispatch(loadGamesActionCreator(gamesData));
+      try {
+        const gamesData = await getGamesApi();
+        dispatch(loadGamesActionCreator(gamesData));
+      } catch (error) {
+        toast.error("Error in loading page");
+      }
     })();
   }, [dispatch, getGamesApi]);
 
   return (
     <HomePageStyled>
       <h1>Games</h1>
+      <ToastContainer
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="colored"
+      />
       <GamesList />
     </HomePageStyled>
   );
