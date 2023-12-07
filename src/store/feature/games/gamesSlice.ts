25d4495262bbc5cab6f3072706b7f9e3c66f60f8
Matias Risco <matiasriscolagos@gmail.com>
2023-12-08 00:22:00 +0100

    Add uiSlice and gameSlice file

---
+++ src/store/feature/games/gamesSlice.ts
@@ -0,0 +1,43 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { GameStateStructure, GameStructure } from "./types";
+import { copyGames } from "./utils";
+
+export const initialGamesState: GameStateStructure = {
+  games: [],
+};
+
+const gamesSlice = createSlice({
+  name: "gameState",
+  initialState: initialGamesState,
+  reducers: {
+    loadGames: (
+      currentState,
+      action: PayloadAction<GameStructure[]>,
+    ): GameStateStructure => ({
+      ...currentState,
+      games: copyGames(action.payload),
+    }),
+
+    deleteGame: (
+      currentState,
+      action: PayloadAction<string>,
+    ): GameStateStructure => ({
+      ...currentState,
+      games: copyGames(
+        currentState.games.filter(({ id }) => id !== action.payload),
+      ),
+    }),
+
+    addGame: (currentState, action: PayloadAction<GameStructure>) => ({
+      ...currentState,
+      games: copyGames([...currentState.games, action.payload]),
+    }),
+  },
+});
+
+export default gamesSlice.reducer;
+export const {
+  loadGames: loadGamesActionCreator,
+  deleteGame: deleteGameActionCreator,
+  addGame: addGameActionCreator,
+} = gamesSlice.actions;
