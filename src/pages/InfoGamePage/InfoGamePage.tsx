4de4ed4651cf057df11d7dba450c59669afc6853
Matias Risco <matiasriscolagos@gmail.com>
2023-12-08 13:52:26 +0100

    Implement navigation to NotFoundPage in InfoGamePage

--- src/pages/InfoGamePage/InfoGamePage.tsx
+++ src/pages/InfoGamePage/InfoGamePage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import InfoGamePageStyled from "./InfoGamePageStyled";
 import { useAppSelector } from "../../store/hooks";
 import { useEffect, useState } from "react";
@@ -26,6 +26,7 @@ const InfoGamePage = (): React.ReactElement => {
   const { games } = useAppSelector(({ gameState }) => gameState);
   const { infoGameApi } = useGameApi();
   const [game, setGame] = useState(initialGame);
+  const [isErrorLoading, setIsErrorLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -41,14 +42,18 @@ const InfoGamePage = (): React.ReactElement => {
         setGame(newGame);
       } catch (error) {
         toast.error("Error game not found");
+        setIsErrorLoading(true);
       }
     })();
   }, [games, idGame, infoGameApi]);
 
+  const isGame: boolean = game.id?.length !== 0;
+
   return (
     <InfoGamePageStyled>
       <h1>Info Game</h1>
-      {game?.id?.length && <GameInfo game={game} />}
+      {isGame && <GameInfo game={game} />}
+      {isErrorLoading && <Navigate to="/game/info-error" />}
     </InfoGamePageStyled>
   );
 };
